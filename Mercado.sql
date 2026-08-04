950111666e5cf14f73b72e8638f5b0a57c602377
FelipeSa <feliperibeirosa077@gmail.com>
2026-08-04 10:56:43 -0300

    Primeiro commit - Banco de Dados

---
+++ Mercado.sql
@@ -0,0 +1,22 @@
+create table usuarios (
+id_usuario int primary key auto_increment,
+nome varchar(30),
+cidade varchar(30)
+);
+
+insert into usuarios(nome,cidade)
+values
+("Felipe", "ribeirao preto"),
+("victor", "ribeirao preto"),
+("ana", "ribeirao preto"),
+("joao", "ribeirao preto"),
+("giovana", "ribeirao preto"),
+("lucas", "ribeirao preto");
+
+use mercado;
+
+select * from usuarios;
+
+delete from usuarios
+where id_usuario = 6
+
